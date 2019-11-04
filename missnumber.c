ff149847b8dc3ec29f4e1e4a9a6fc3ba6bab809c
SmilealniS <52380835+SmilealniS@users.noreply.github.com>
2019-11-04 19:31:18 +0700

    Add files via upload

---
+++ missnumber.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, swap;
+
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i=0; i<n-1; i++)
+    {
+        for(int j=0; j<n-i-1; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                swap=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=swap;
+            }
+        }
+    }
+
+    for(int i=0, j=1; i<n; i++, j++)
+    {
+        if(arr[i]!=j && arr[i]>=arr[0] && arr[i]<=arr[n-1])
+        {
+            printf("%d ", j);
+            i--;
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
